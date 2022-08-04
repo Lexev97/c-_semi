2a9912b7919683c704288667c04a68592bb5d8d7
Alex Cap <jamurmansk@yandex.ru>
2022-08-04 21:31:20 +0300

    tasks 1, 3 are done

--- Semi_1/Program.cs
+++ Semi_1/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // int - значения от - 2 147 483 648 до 2 147 483 647
+int square = number * number;
+Console.WriteLine("Квадрат введенного числа: " + square);
+// или
+Console.WriteLine($"Квадрат введенного числа: {number} = {square}");
+
+if (number == 5 && number ==7)
+{
+
+}
+else if (number == 6 || number != 3) 
+{
+
+}
+else
+{
+
+}
---
+++ Semi_1_3/Program.cs
@@ -0,0 +1,42 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Введите номер дня недели: ");
+int daynum = Convert.ToInt32(Console.ReadLine());
+
+if (daynum == 1) 
+{
+    Console.WriteLine("Понедельник");
+}
+else if (daynum == 2) 
+{
+    Console.WriteLine("Вторник");
+}
+else if (daynum == 3) 
+{
+    Console.WriteLine("Среда");
+}
+else if (daynum == 4) 
+{
+    Console.WriteLine("Четверг");
+}
+else if (daynum == 5) 
+{
+    Console.WriteLine("Пятница");
+}
+else if (daynum == 6) 
+{
+    Console.WriteLine("Суббота");
+}
+else if (daynum == 7) 
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+
+
