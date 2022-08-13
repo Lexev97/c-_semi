fd870f0f3a8db2cdbdfe2b6a9d50f9f93feb1449
Alex Cap <jamurmansk@yandex.ru>
2022-08-13 23:04:58 +0300

    HW. Task 21 is done.

---
+++ Semi_3_HW_21/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите 2-х точек:");
+Console.Write("х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+double HipDim (int a1, int a2, int a3, int b1, int b2, int b3)
+{
+    return Math.Sqrt( (a1 - b1) * (a1 - b1) + (a2 - b2) * (a2 - b2) + (a3 - b3) * (a3 - b3));
+}
+
+double result = HipDim (x1, y1, z1, x2, y2, z2);
+Console.WriteLine("Расстояние между точками составляет: {0: #.##}", result);
