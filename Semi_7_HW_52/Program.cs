91848e617a9052e749ebc7a154e29ee8f38a6c7c
Alex Cap <jamurmansk@yandex.ru>
2022-08-27 23:39:27 +0300

    hw task 52 is done

---
+++ Semi_7_HW_52/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int cols, int min, int max)
+{
+    int[,] matrix = new int[rows, cols];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+        
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            // else Console.Write($"{matrix[i, j], 4}");
+
+            Console.Write (j < matrix.GetLength(1) - 1 ? 
+            $"{matrix[i, j], 4}, " : $"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("]");
+    }
+    
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write(array[i]);
+    }
+}
+
+double[] AverageOfColumn(int[,] matrix)
+{
+    double[] average = new double[matrix.GetLength(1)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            average[j] += matrix[i, j]; 
+        }
+    }
+
+    for (int i = 0; i < average.Length; i++)
+    {
+        average[i] = Math.Round(average[i] / matrix.GetLength(0), 1, MidpointRounding.ToEven);
+    }
+
+    return average;
+}
+
+int[,] arr2d = CreateMatrixRndInt(3, 4, 1, 9);
+double[] averageArr = AverageOfColumn (arr2d);
+PrintMatrix(arr2d);
+
+Console.Write($"Среднее арифметическое каждого столбца: ");
+PrintArray(averageArr);
+
