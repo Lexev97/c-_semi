2afee11b0b59ac3d7771ecc581b473633d15e07c
Alex Cap <jamurmansk@yandex.ru>
2022-09-03 14:48:01 +0300

    HW task 54 is done

---
+++ Semi_8_HW_54/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int[,] CreateMatrixRndInt(int rows, int cols, int min, int max)
+{
+    int[,] matrix = new int[rows, cols];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+        
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            // else Console.Write($"{matrix[i, j], 4}");
+
+            Console.Write (j < matrix.GetLength(1) - 1 ? 
+            $"{matrix[i, j], 4}, " : $"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("]");
+    }
+    
+}
+
+void OrderMatrixRowsMaxMin(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+   
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            int maxPosition = j;
+
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, maxPosition]) maxPosition = k;
+            }
+
+            int tempVal = matrix[i, j];
+            matrix[i, j] = matrix[i, maxPosition];
+            matrix[i, maxPosition] = tempVal;
+        }
+        
+    }
+}
+
+int[,] arr2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(arr2d);
+OrderMatrixRowsMaxMin(arr2d);
+Console.WriteLine();
+PrintMatrix(arr2d);
