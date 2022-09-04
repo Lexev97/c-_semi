8b4b2a7856219be996c2c6fef7ed43f7324941e1
Alex Cap <jamurmansk@yandex.ru>
2022-09-04 17:45:02 +0300

    HW task 66 is done

---
+++ Semi_9_HW_66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите 2 целых положительных числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+int SumDigits(int num1, int num2)
+{
+    int sum = num1;
+    if (num1 == num2) return num1;
+    return sum += SumDigits(num1 + 1, num2);
+}
+
+Console.WriteLine(a > b ? SumDigits(b, a) : SumDigits(a, b));
